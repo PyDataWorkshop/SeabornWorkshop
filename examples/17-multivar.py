ae9682c96cf21a1688b5ab2cfbd3aa72e2478159
DragonflyStats <kobriendublin@gmail.com>
2017-05-15 08:45:18 +0100

    Delete 17-multivar.py

--- examples/17-multivar.py
+++
@@ -1,4 +0,0 @@
-sample = np.random.multivariate_normal([0, 0], [[1, -.5], [-.5, 1]], size=100)
-pal = sns.dark_palette("palegreen", as_cmap=True)
-plt.figure(figsize=(6, 6))
-sns.kdeplot(sample, cmap=pal);
